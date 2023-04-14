306402d327676ea1e7dd76b0081cfff5a0d9569b
Feven-Demoz <130766141+Feven-Demoz@users.noreply.github.com>
2023-04-14 10:05:50 -0400

    HW1 Command Line Argument

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) { //passing array of strings as a parameter in the main function
+        System.out.println("The first argument entered is " + args[0]); // printing and accessing the first command line argument
+        System.out.println("The second argument entered is " + args[1]);  // printing and accessing the second command line argument
+    }
+}
