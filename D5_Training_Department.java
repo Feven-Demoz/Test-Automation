ee687fcd32c453b53c7525f22583028add366a3c
Feven-Demoz <130766141+Feven-Demoz@users.noreply.github.com>
2023-04-21 12:02:10 -0400

    Delete D5_Training_Department.java

--- D5_Training_Department.java
+++
@@ -1,9 +0,0 @@
-public class D5_Training_Department {
-    String new_employee;
-    String Department;
-
-    public D5_Training_Department(String new_employee_enter, String Department_enter) {
-        this.new_employee = new_employee_enter;
-        this.Department = Department_enter;
-    }
-}
