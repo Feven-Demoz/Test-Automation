fe11877b832a5eabe5cc317bb04557b12bd97ce6
Feven-Demoz <130766141+Feven-Demoz@users.noreply.github.com>
2023-04-25 11:27:16 -0400

    Delete EngineeringDepartment.java

--- EngineeringDepartment.java
+++
@@ -1,9 +0,0 @@
-public interface EngineeringDepartment {
-
-   void setEngineeringTeams(String engineeringTeamsEnter);
-   String getEngineeringTeams();
-    void setYearsOfExperience(int yearsOfExperienceEnter);  // No function body needed
-    int getYearsOfExperience();
-
-
-}
