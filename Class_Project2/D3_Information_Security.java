20080e9ae0fc61ca3185d399003cbd122757a60d
Feven-Demoz <130766141+Feven-Demoz@users.noreply.github.com>
2023-04-25 11:22:41 -0400

    Delete D3_Information_Security.java

--- Class_Project2/D3_Information_Security.java
+++
@@ -1,9 +0,0 @@
-public class D3_Information_Security {
-String division;
-String group;
-
-    public D3_Information_Security(String division_enter, String group_enter) {
-        this.division = division_enter;
-        this.group = group_enter;
-    }
-}
