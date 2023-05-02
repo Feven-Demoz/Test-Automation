0d74471a954cb19f53ac97a70f27cd954b87f796
Feven-Demoz <130766141+Feven-Demoz@users.noreply.github.com>
2023-05-02 10:40:36 -0400

    Delete SoftwareDevelopmentDepartment.java

--- HomeWork6/SoftwareDevelopmentDepartment.java
+++
@@ -1,6 +0,0 @@
-public interface SoftwareDevelopmentDepartment {
-    public void setProject ( String project );
-    String getProject();
-    public void setProgramingLanguage( String programingLanguage );
-    String getProgramingLanguage();
-}
